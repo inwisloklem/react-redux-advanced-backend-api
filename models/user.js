575e1a4bcc585a87425ea9a8dce2e34d18429249
inwisloklem <89099109104@mail.ru>
2019-06-19 21:25:59 +0300

    Update (115)

--- models/user.js
+++ models/user.js
@@ -10,9 +10,7 @@ userSchema.pre('save', async function (next) {
   const user = this
 
   try {
-    const salt = await bcrypt.genSalt(10)
-    const hash = await bcrypt.hash(user.password, salt)
-    user.password = hash
+    user.password = await bcrypt.hash(user.password, 12)
     next()
   } catch (err) {
     return next(err)
